f0a448f803a3916727f6df57e0504c705a410d13
Akinwale Samuel <wale0331@gmail.com>
2019-09-27 17:15:04 +0100

    add sidebar content

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,12 +1,45 @@
 // react
 import React from 'react';
-// components
 // style
 import './style.scss';
+// image
+import dashboard from '../../images/dashboard1.svg';
+import balances from '../../images/balances.svg';
+import payment from '../../images/payment.svg';
 
 const Sidebar = () => (
-  <div className="sidebar">
-    <p>Sidebar</p>
+  <div>
+    <div className="sidebar">
+      <div className="link">
+        <a href="/">
+          <img src={dashboard} alt="dashboard"/> Dashboard
+        </a>
+      </div>
+      <div className="link">
+        <a href="/">
+          <img src={balances} alt="dashboard"/> Payments
+        </a>
+      </div>
+      <div className="link">
+        <a href="/">
+          <img src={payment} alt="dashboard"/> Transfers
+        </a>
+      </div>
+      <div className="link">
+        <a href="/">
+          <img src={payment} alt="dashboard"/> Developers
+        </a>
+      </div>
+      <div className="link">
+        <a href="/">
+          <img src={payment} alt="dashboard"/> Settings
+        </a>
+      </div>
+    </div>
+    <div className="payment">
+      <i class="fas fa-plus"></i>
+      <p>Accept payment</p>
+    </div>
   </div>
 );
 
